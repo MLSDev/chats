df2ffdb589fb71954570ee2908ca2709f273b9b4
butumusho <danuljak@gmail.com>
2016-07-22 11:36:31 +0300

    Overrided create action.

--- app/controllers/profiles_controller.rb
+++ app/controllers/profiles_controller.rb
@@ -3,6 +3,22 @@ class ProfilesController < ApplicationController
 
   before_action :build_resource, only: :create
 
+  def create
+    respond_to do |format|
+      format.html do
+        if resource.save
+          redirect_to profile_path(@user.id)
+        else
+          render :new
+        end
+      end
+
+      format.js { render :errors unless resource.save }
+
+      format.json { render :errors unless resource.save }
+    end
+  end
+
   private
   def build_resource
     @user = User.new resource_params
